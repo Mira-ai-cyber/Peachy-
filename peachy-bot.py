0495feff434f1b78df05cdffce64df82ed3fb2cf
Mira-ai-cyber <atour0905@gmail.com>
2025-04-22 14:19:48 -0700

    Update1 peachy-bot.py

--- peachy-bot.py
+++ peachy-bot.py
@@ -15,9 +15,6 @@ LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
